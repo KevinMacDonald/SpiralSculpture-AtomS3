1799cd9983f36da69ad7a3600a6788c31324a6fa
KevinMacDonald <kevinmacdonald@gmail.com>
2026-02-04 23:21:22 -0800

    motor now always turns with light change, but still only one direction

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,7 @@
 #include <Arduino.h>
 
 #define LED_PIN 35
-// Motor Pins for Atom H-Driver (IN1/IN2 Mode)
-// AtomS3 Lite Bottom Header: G5 and G6
-#define MOTOR_IN1 5
-#define MOTOR_IN2 6
+
 // Motor Pins for Atom H-Driver (PH/EN Mode)
 // Based on testing: G6 is Enable (Speed), G5 is Phase (Direction)
 #define PIN_ENABLE 6
@@ -14,8 +11,6 @@
 // PWM Settings
 #define PWM_FREQ 30000 // 30kHz to reduce motor noise
 #define PWM_RES 8      // 8-bit resolution (0-255)
-#define PWM_CH1 0
-#define PWM_CH2 1
 #define PWM_CH_ENABLE 0
 
 void setup() {
@@ -23,34 +18,23 @@ void setup() {
     cfg.serial_baudrate = 115200; // M5Unified will handle Serial.begin
     M5.begin(cfg);
 
-    // Setup Motor PWM channels
-    ledcSetup(PWM_CH1, PWM_FREQ, PWM_RES);
-    ledcSetup(PWM_CH2, PWM_FREQ, PWM_RES);
     // Setup Enable Pin (PWM for Speed)
     ledcSetup(PWM_CH_ENABLE, PWM_FREQ, PWM_RES);
     ledcAttachPin(PIN_ENABLE, PWM_CH_ENABLE);
     
-    // Attach pins to channels
-    ledcAttachPin(MOTOR_IN1, PWM_CH1);
-    ledcAttachPin(MOTOR_IN2, PWM_CH2);
     // Setup Phase Pin (Digital for Direction)
     pinMode(PIN_PHASE, OUTPUT);
     
-    Serial.println("AtomS3 Lite Motor Control: IN1/IN2 Mode");
     Serial.println("AtomS3 Lite Motor Control: PH/EN Mode Corrected");
 }
 
 void loop() {
     M5.update(); // Update button states
 
-    // 1. Rotate Direction A
-    Serial.println("Direction A (IN1=PWM, IN2=LOW)");
     // 1. Rotate Direction A (Clockwise)
     Serial.println("Direction A (PH=HIGH, EN=PWM)");
     neopixelWrite(LED_PIN, 0, 20, 0); // Green
     
-    ledcWrite(PWM_CH1, 200); // Speed
-    ledcWrite(PWM_CH2, 0);   // Hold Low
     digitalWrite(PIN_PHASE, HIGH);   // Set Direction A
     ledcWrite(PWM_CH_ENABLE, 200);   // Set Speed
     delay(3000);
@@ -59,19 +43,13 @@ void loop() {
     Serial.println("Stopping");
     neopixelWrite(LED_PIN, 20, 0, 0); // Red
     
-    ledcWrite(PWM_CH1, 0);
-    ledcWrite(PWM_CH2, 0);
     ledcWrite(PWM_CH_ENABLE, 0);     // Stop
     delay(2000);
 
-    // 3. Rotate Direction B
-    Serial.println("Direction B (IN1=LOW, IN2=PWM)");
     // 3. Rotate Direction B (Counter-Clockwise)
     Serial.println("Direction B (PH=LOW, EN=PWM)");
     neopixelWrite(LED_PIN, 0, 0, 20); // Blue
     
-    ledcWrite(PWM_CH1, 0);   // Hold Low
-    ledcWrite(PWM_CH2, 200); // Speed
     digitalWrite(PIN_PHASE, LOW);    // Set Direction B
     ledcWrite(PWM_CH_ENABLE, 200);   // Set Speed
     delay(3000);
@@ -80,8 +58,6 @@ void loop() {
     Serial.println("Stopping");
     neopixelWrite(LED_PIN, 20, 0, 0); // Red
     
-    ledcWrite(PWM_CH1, 0);
-    ledcWrite(PWM_CH2, 0);
     ledcWrite(PWM_CH_ENABLE, 0);     // Stop
     delay(2000);
 }
