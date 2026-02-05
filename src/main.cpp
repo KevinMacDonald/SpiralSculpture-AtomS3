e7459ae892ba8c47dc3c459451fc9374d09358ad
KevinMacDonald <kevinmacdonald@gmail.com>
2026-02-05 14:42:46 -0800

    Got one motor direction working

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include <M5Unified.h>
+#include <Arduino.h>
 
 #define LED_PIN 35
 
 // Atomic H-Driver Pin Definitions
-const int IN1_PIN = 6;
-const int IN2_PIN = 7;
+const int IN1_PIN = 5;
+const int IN2_PIN = 6;
 
 // PWM Settings
 const int freq = 10000;
-const int resolution = 10; // 0-1023
-const int ledChannel1 = 0;
-const int ledChannel2 = 1;
+const int resolution = 8; // 0-1023
+const int ledChannel1 = 4;
+const int ledChannel2 = 5;
 
 bool direction = true;
 int counter = 0;
@@ -19,6 +20,11 @@ void setup() {
     auto cfg = M5.config();
     cfg.serial_baudrate = 115200;
     M5.begin(cfg);
+
+    // Reset pins to ensure no other peripheral is holding them
+    gpio_reset_pin((gpio_num_t)IN1_PIN);
+    gpio_reset_pin((gpio_num_t)IN2_PIN);
+
     delay(1000); // Give serial monitor time to connect
 
     Serial.println("System Initialized");
@@ -39,26 +45,43 @@ void loop() {
 
     counter++;
 
-    Serial.println("bbbMy Loop:" + String(counter));
-
+    if (counter % 10 == 0) {
+        Serial.println("My Loop:" + String(counter));
+    }
 
     // Toggle motor on button press
     if (M5.BtnA.wasPressed()) {
         if (direction) {
-            neopixelWrite(LED_PIN, 0, 0, 50); 
-            ledcWrite(ledChannel1, 500); // 50% duty cycle Forward
-            ledcWrite(ledChannel2, 0);
+            neopixelWrite(LED_PIN, 0, 0, 50); //Blue
+
+            for (int speed = 50; speed <= 255; speed++) {
+                ledcWrite(ledChannel1, 0);
+                ledcWrite(ledChannel2, speed);
+                delay(10);
+            }
+            delay(1000); // Hold max speed
+
+            // --- 4. Decelerate Counter-Clockwise ---
+            Serial.println("Decelerating CCW");
+            for (int speed = 255; speed >= 0; speed--) {
+                ledcWrite(ledChannel1, 0);
+                ledcWrite(ledChannel2, speed);
+                delay(10);
+            }
+
+            //ledcWrite(ledChannel1, 50); // 50% duty cycle Forward
+            //ledcWrite(ledChannel2, 0);
         } else {
-            neopixelWrite(LED_PIN, 0, 50, 0); 
+            neopixelWrite(LED_PIN, 0, 50, 0); //Green
             ledcWrite(ledChannel1, 0);
-            ledcWrite(ledChannel2, 500); // 50% duty cycle Reverse
+            ledcWrite(ledChannel2, 50); // 50% duty cycle Reverse
         }
         direction = !direction;
     }
 
     // Stop motor on long press
     if (M5.BtnA.pressedFor(2000)) {
-        neopixelWrite(LED_PIN, 50, 0, 0); 
+        neopixelWrite(LED_PIN, 50, 0, 0); //Red
         ledcWrite(ledChannel1, 0);
         ledcWrite(ledChannel2, 0);
     }
